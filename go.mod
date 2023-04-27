547b4f1ee7afd1a1cb5c57d3325b0bd8434d13e6
Lasavlad <98495511+Lasavlad@users.noreply.github.com>
2023-04-27 15:39:41 +0100

    basic server complete

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Lasavlad/webserver_golang
+
+go 1.20
--- main.go
+++ main.go
@@ -6,6 +6,42 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil{
+		fmt.Fprintf(w, "ParseForm() err %v", err)
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "NAME = %s\n", name)
+	fmt.Fprintf(w, "ADDRESS= %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET"{
+		http.Error(w, "method is not supported", http.StatusNotFound)
+
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
+
 func main(){
-	
+	// to check out the static directory 
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+	//
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello",helloHandler)
+
+	fmt.Printf("stating server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil{
+		log.Fatal(err)
+	}
 }
